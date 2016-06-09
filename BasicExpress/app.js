378270634113db34fd22ba108fd740449098a8d9
Tommy Caito <tcaito31@gmail.com>
2016-06-09 14:56:05 -0400

    Finish BasicExpress project (project 2)

--- BasicExpress/app.js
+++ BasicExpress/app.js
@@ -8,12 +8,56 @@ const port = 1337;
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 // body-parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.render('index', {'title': 'Index'});
+});
+
+app.get('/about', function(req, res){
+  res.render('about', {'title': 'About'});
+});
+
+app.get('/services', function(req, res){
+  res.render('services', {'title': 'Services'});
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact', {'title': 'Contact Us'});
+});
+
+app.post('/contact/send', function(req, res){
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'tcaito31@gmail.com',
+      pass: 'gfnznatrzdvvxtth'
+    }
+  });
+
+  var mailOptions = {
+    from: 'Tommy Caito <tcaito31@gmail.com>',
+    to: 'tcaito@umich.edu',
+    subject: 'Website Submission',
+    text: 'You have a submission with the following details... \nName: ' + req.body.name + '\nEmail: ' + req.body.email + '\nMessage: ' + req.body.message,
+    html: '<p>You have a submission with the following details...</p><ul><li>Name: ' + req.body.name + '</li><li>Email: ' + req.body.email + '</li><li>Message: ' + req.body.message + '</li></ul>'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log(error);
+      res.redirect('/');
+    } else{
+      console.log('Message Sent: ' + info.response);
+      res.redirect('/');
+    }
+  })
 });
 
 app.listen(port);
